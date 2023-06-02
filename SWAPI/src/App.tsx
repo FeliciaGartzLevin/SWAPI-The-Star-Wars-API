bfb92d44c08beb41999e28cfc89ba35f8fa7a557
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-06-02 21:56:19 +0200

    tried to get a favicon.icon, without background, but no success yet

--- SWAPI/src/App.tsx
+++ SWAPI/src/App.tsx
@@ -1,10 +1,27 @@
-import App from '../src/assets/scss'
+import Container from 'react-bootstrap/Container'
+import { Routes, Route } from 'react-router-dom'
+// import Navigation from './components/Navigation'
+// import NotFound from './pages/NotFound'
+// import HomePage from './pages/HomePage'
+// import SearchPage from './pages/SearchPage'
+import './assets/scss/App.scss'
 
 const App = () => {
-  <div id="App">
-    <>
-    </>
-  </div>
+
+  return (
+    <div id="App">
+      {/* <Navigation /> */}
+
+      <Container className='py-3'>
+        <Routes>
+          {/* <Route path='/' element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+
+          <Route path='/*' element={<NotFound />} /> */}
+        </Routes>
+      </Container>
+    </div>
+  )
 }
 
 export default App
