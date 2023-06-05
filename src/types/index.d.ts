98141041f32e66ad595373d24c5ac328a6d1b41e
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-06-05 19:54:58 +0200

    updated Films interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -16,7 +16,23 @@ export interface Film {
 	species_count: number,
 }
 
+interface Link {
+	url: string | null,
+	label: string,
+	active: boolean
+}
 export interface Films {
 	current_page: number,
 	data: Film[]
+	first_page_url: string
+	from: number,
+	last_page: number,
+	last_page_url: string,
+	links: Link[]
+	next_page_url: string | null,
+	path: string,
+	per_page: number,
+	prev_page_url: string | null,
+	to: number,
+	total: number,
 }
